d65655ab381c4fdfe3a127f1744dea03d59d82f0
Mihnea Ionita <dev@imihnea.com>
2020-10-27 18:04:46 +0100

    Added functions for each game state

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,26 @@ package main.java;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Wild Llama Entertainment was here.");
+
+        showMainMenu();
+        playGame();
+        movePlayer();
+        endGame();
+    }
+
+    private static void showMainMenu() {
+        System.out.println("Show main menu");
+    }
+
+    private static void playGame() {
+        System.out.println("Show game window");
+    }
+
+    private static void movePlayer() {
+        System.out.println("Moving player");
+    }
+
+    private static void endGame() {
+        System.out.println("Show end game");
     }
 }
