84f5a559fcf9d8c8699d4f721c8acce9ad9ecc00
Mihnea Ionita <dev@imihnea.com>
2020-10-30 00:04:08 +0100

    Fixed error with endgame code and recompiled

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -132,7 +132,6 @@ public class Main {
                 "                                                   \n" +
                 "                                                   ";
         System.out.print(EndGame + "\n");
-        }
     }
 }
 
