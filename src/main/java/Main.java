7824b2d2d26fddd85da1c460b1c6147839fc8de7
Mihnea Ionita <dev@imihnea.com>
2020-10-29 19:05:40 +0100

    Added Julia's code for the showMenu function

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,12 +15,13 @@ public class Main {
 
     public static void main(String[] args) {
         showMainMenu();
-        playGame();
+        if(!gameOver) {
+            playGame();
+        }
         endGame();
     }
 
     private static void showMainMenu() {
-        System.out.println("Show main menu");
         /*
         String GameName = "  ____  ____    ___   ___ ___    ___        ___  __ __  ____  _       ___   ____     ___  ____    _____\n" +
                 " /    T|    \\  /   \\ |   T   T  /  _]      /  _]|  T  T|    \\| T     /   \\ |    \\   /  _]|    \\  / ___/\n" +
@@ -39,6 +40,39 @@ public class Main {
         //Maybe we should add a start button
         */
 
+        String playerName;
+
+        int choice;
+
+        System.out.println("Wild Llama Entertainment proudly presents..");
+        System.out.println("  ____  ____    ___   ___ ___    ___        ___  __ __  ____  _       ___   ____     ___  ____    _____\n" +
+                " /    T|    \\  /   \\ |   T   T  /  _]      /  _]|  T  T|    \\| T     /   \\ |    \\   /  _]|    \\  / ___/\n" +
+                "Y   __j|  _  YY     Y| _   _ | /  [_      /  [_ |  |  ||  o  ) |    Y     Y|  D  ) /  [_ |  D  )(   \\_ \n" +
+                "|  T  ||  |  ||  O  ||  \\_/  |Y    _]    Y    _]l_   _j|   _/| l___ |  O  ||    / Y    _]|    /  \\__  T\n" +
+                "|  l_ ||  |  ||     ||   |   ||   [_     |   [_ |     ||  |  |     T|     ||    \\ |   [_ |    \\  /  \\ |\n" +
+                "|     ||  |  |l     !|   |   ||     T    |     T|  |  ||  |  |     |l     !|  .  Y|     T|  .  Y \\    |\n" +
+                "l___,_jl__j__j \\___/ l___j___jl_____j    l_____j|__j__|l__j  l_____j \\___/ l__j\\_jl_____jl__j\\_j  \\___j");
+
+        System.out.println("\n\tNew Game\n Press 1 to continue\n Press 2 to exit");
+
+        choice = keyboard.nextInt();
+
+        if (choice == 2) {
+            System.out.println("Untraveled paths dismay the frail. Return with more valiance.");
+            gameOver = true;
+            return;
+        } else if (choice == 1) {
+            System.out.println("\tEnter your name");
+        } else {
+            System.out.println("\tTry another input next time");
+            return;
+        }
+
+        playerName = keyboard.next();
+
+        System.out.println("Prepare, " + playerName + ", for adventure awaits you...");
+
+        // "Enter your name' isn't aligned with "new game" and I can't figure out how to use souf or anything else to fix this :/
     }
 
 
@@ -79,7 +113,6 @@ public class Main {
 
             // Sample game over condition
             if(x == 5 || y == 5 || x == -5 || y == -5) {
-                System.out.println("You've strayed too far from the light..");
                 gameOver = true;
             }
         }
