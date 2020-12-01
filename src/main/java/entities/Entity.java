0f5594a3dec09912c8dd6b9f0230a57d80b2778f
Mihnea Ionita <dev@imihnea.com>
2020-12-01 13:36:46 +0100

    Entity and animal classes inside a new package called entities

---
+++ src/main/java/entities/Entity.java
@@ -0,0 +1,38 @@
+package main.java.entities;
+
+import java.awt.*;
+
+public class Entity {
+    protected int x;
+    protected int y;
+
+    protected String type;
+    protected char symbol;
+    protected Color color;
+
+    // Standard getters for the properties of an entity
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+    public char getSymbol() {
+        return this.symbol;
+    }
+
+    public String getType() {
+        return type;
+    }
+    public Color getColor() {
+        return this.color;
+    }
+
+    public Entity(String name, char symbol, Color color, int xPos, int yPos) {
+        this.type = name;
+        this.symbol = symbol;
+        this.color = color;
+        this.x = xPos;
+        this.y = yPos;
+    }
+}
