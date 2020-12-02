fba6cc67b73d3181d4777622b2dd2c1146604e21
Mihnea Ionita <dev@imihnea.com>
2020-12-02 17:28:49 +0100

    added missing code from WinWindow similar to losewindow

--- src/main/java/ui/WinWindow.java
+++ src/main/java/ui/WinWindow.java
@@ -8,17 +8,28 @@ import static main.java.Main.endGame;
 
 public class WinWindow implements Window {
 
+    private static boolean addedToDb = false;
+
     public void displayOutput(AsciiPanel terminal) {
         terminal.write("The gnomes found the way out! You have won!", 1, 1);
         terminal.writeCenter("Press [ENTER] to play again or [ESC] to go to main menu", 22);
-        endGame();
+        if (!addedToDb) {
+            addedToDb = true;
+            endGame();
+        }
+    }
+
+    private static void resetVariables() {
+        addedToDb = false;
     }
 
     public Window respondToUserInput(KeyEvent key) {
         switch (key.getKeyCode()) {
             case KeyEvent.VK_ESCAPE:
+                resetVariables();
                 return new StartWindow();
             case KeyEvent.VK_ENTER:
+                resetVariables();
                 return new PlayWindow();
             default:
                 return this;
