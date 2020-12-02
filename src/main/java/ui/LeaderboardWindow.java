3aa0eceb64b3ee04a078dff30513177c30c2b635
Ruben Brouwers <ruben.brouwers@student.kdg.be>
2020-12-02 17:31:31 +0100

    Added Leaderbord graphics, and aligned company name

--- src/main/java/ui/LeaderboardWindow.java
+++ src/main/java/ui/LeaderboardWindow.java
@@ -12,7 +12,9 @@ import static main.java.Main.statement;
 public class LeaderboardWindow implements Window {
 
     public void displayOutput(AsciiPanel terminal) {
-        terminal.writeCenter("Gnome Explorers Leaderboard", 1);
+        terminal.writeCenter("-------------------------------", 1);
+        terminal.writeCenter("| Gnome Explorers Leaderboard |", 2);
+        terminal.writeCenter("-------------------------------",3);
 
         try {
             ResultSet resultSet = statement.executeQuery("SELECT * FROM (SELECT player_name, end_time, score FROM INT_leaderboard ORDER BY score DESC) WHERE ROWNUM <= 5");
@@ -24,13 +26,18 @@ public class LeaderboardWindow implements Window {
                 Timestamp time = resultSet.getTimestamp(2);
                 int score = resultSet.getInt(3);
 
-                terminal.writeCenter(i + ". " + name + "     " + time + "     " + score, 2 + i);
+                terminal.writeCenter(i + ". " + name + "     " + time + "     " + score, 5 + i);
 
                 i++;
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
+        terminal.writeCenter("  .__.", 13);
+        terminal.writeCenter("  (|  |)",14);
+        terminal.writeCenter("  (  )", 15);
+        terminal.writeCenter("  _)(_", 16);
 
         terminal.writeCenter("Press [ENTER] to return to main menu", 22);
     }
