3f47d386066ba6e81d11ae2c804977dcaeaecb78
Mihnea Ionita <dev@imihnea.com>
2020-12-29 17:08:22 +0100

    Level reader and walls array generating

--- src/main/java/world/World.java
+++ src/main/java/world/World.java
@@ -2,8 +2,51 @@ package main.java.world;
 
 import asciiPanel.AsciiPanel;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class World {
     public void generateWorld(AsciiPanel terminal) {
-        terminal.write('#', 3,3);
+        ArrayList<int[]> walls = new ArrayList<int[]>();
+        int position[] = new int[2];
+
+        try (BufferedReader br = new BufferedReader(new FileReader("resources/floors/floor1.txt"))) {
+            String s;
+            String line = br.readLine();
+
+            int lineIndex = 0;
+
+            while (line != null) {
+
+                lineIndex++;
+
+                System.out.println("line: " + line + "\n");
+
+                s = line;
+
+                // convert String to char[] array
+                char[] chars = s.toCharArray();
+
+                // iterate over char[] array using enhanced for loop
+                int charIndex = 0;
+                for (char ch : chars) {
+                    position[0] = charIndex;
+                    position[1] = lineIndex;
+                    System.out.print("char: " + ch + " position: " + Arrays.toString(position) + "\n");
+                    if (ch == '1') {
+                        walls.add(position);
+                    }
+                    charIndex++;
+                }
+
+                line = br.readLine();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
